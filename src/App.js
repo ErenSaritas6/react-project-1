492678a2c07053e95d0a1c760b440eb7efc5e664
Eren Sarıtaş <79216477+ErenSaritas6@users.noreply.github.com>
2023-04-06 16:45:44 +0300

    List app
    list app finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import Adduser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
 
 function App() {
+
+const [usersList, setUsersList] = useState([]);
+
+const addUserHandler = (uName, uAge, uColor) => {
+  setUsersList((prevUsersList) => {
+    return [
+      ...prevUsersList, 
+      {name: uName, age: uAge, color: uColor, id: Math.random().toString()},
+    ]
+  });
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Adduser onAddUser={addUserHandler}/>
+        <UserList users={usersList}/>
     </div>
   );
 }
